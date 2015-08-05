1a3ba0a53ef8f9bdc41e79c56bdcc74372d292d5
Diogo Pinto <h.diogopinto@live.com>
2015-08-05 17:44:34 +0000

    Added nav styles and active page tracking

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Toronto Area Contractor | Uncle Frank\'s Construction' });
+  res.redirect('/home');
+});
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'About | Uncle Frank\'s Construction', page: req.url });
+});
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About | Uncle Frank\'s Construction', page: req.url });
+});
+router.get('/gallery', function(req, res, next) {
+  res.render('index', { title: 'Gallery | Uncle Frank\'s Construction', page: req.url });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Us | Uncle Frank\'s Construction', page: req.url });
 });
 
 module.exports = router;
